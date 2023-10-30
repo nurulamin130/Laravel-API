2e3e363de77393b7ae2d3ea84f294cf26aa4e470
nurulamin130 <82229429+nurulamin130@users.noreply.github.com>
2023-10-30 21:57:23 +0600

    CRUD Operation Successfully

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('students',[StudentController::class,'index']);
 Route::post('students',[StudentController::class,'store']);
+Route::get('students/{id}',[StudentController::class,'show']);
+Route::get('students/{id}/edit',[StudentController::class,'edit']);
+Route::put('students/{id}/edit',[StudentController::class,'update']);
+Route::delete('students/{id}/delete',[StudentController::class,'destroy']);
